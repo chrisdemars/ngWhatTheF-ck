f620a532e2548cf834f8a785a27d2e896cfe6a51
Chris DeMars <cdemars86@gmail.com>
2016-05-18 19:14:07 -0400

    all the frustration

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp              = require('gulp');
+    sass              = require('gulp-sass');
+    cssmin            = require('gulp-cssmin');
+    rename            = require('gulp-rename');
+    prefix            = require('gulp-autoprefixer');
+    browserSync       = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch('src/scss/*.scss', ['sass']);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(prefix('last 2 versions'))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/css'));
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/scss/*.scss', ['sass']);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['sass', 'serve']);
