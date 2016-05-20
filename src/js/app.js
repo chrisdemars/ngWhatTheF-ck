ffb17d4697af554b19eaccf471f98db235ae14d7
Chris DeMars <cdemars86@gmail.com>
2016-05-20 10:41:38 -0400

    mad

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// Declaring an array with ng strings.
+
+var ngWTF = [
+  'ngWhatTheFuck',
+  'ngIHateYou',
+  'ngDammit'
+];
+
+ngWTF = ngWTF[Math.floor(Math.random() * ngWTF.length)];
+
+console.log(ngWTF);
