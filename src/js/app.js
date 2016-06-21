29dafb35ba6487d6c95b0aa72a851dce97a547a3
Chris DeMars <cdemars86@gmail.com>
2016-06-21 19:51:56 -0400

    src files

---
+++ src/js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+
+// Event listener for button click.
+var btn = document.getElementById('button');
+
+btn.onclick = function () {
+
+
+  // Declaring an array with ng strings.
+  var ngStuff = [
+    'ngWhatTheF*ck',
+    'ngArgh',
+    'ngOhNo',
+    'ngIHateYou',
+    'ngDammit',
+    'ngYouSuck',
+    'ngYouAreWorthless',
+    'ngIHateCode',
+    'ngAngularSucks',
+    'ngICantAngular',
+    'ngAngularWhat?',
+    'ngMad',
+    'ngF*ckThis',
+    'ngF*ckThat',
+    'ngF*ckng',
+    'ngF*ckAngular',
+    'ngIHateJavaScript',
+    'ngIHateJS',
+    'ngIHateAngular',
+    'ngSh*t',
+    'ngDamn',
+    'ngCantCode',
+    'ngDevLifeSucks'
+  ];
+  console.log(ngStuff);
+  // Randomizes the array and puts a one of the values in to the generate ID.
+  var random = ngStuff[Math.floor(Math.random() * ngStuff.length)];
+    document.getElementById('generate').innerHTML = random;
+};
