f5d2a47a4f2eac90a5946badf172e8f65d99cf26
Chris DeMars <cdemars86@gmail.com>
2020-02-16 12:03:00 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,12 @@ var gulp              = require('gulp'),
     uglify            = require('gulp-uglify'),
     concat            = require('gulp-concat'),
     rename            = require('gulp-rename'),
-    browserSync       = require('browser-sync').create();
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'js'], function() {
-
-    browserSync.init({
-        server: './',
-        browser: "google chrome canary",
-        open: false,
-    });
+gulp.task(['sass', 'js'], function() {
 
     gulp.watch('src/scss/**/*.scss', ['sass']);
     gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
 // Configure CSS tasks.
@@ -32,7 +24,6 @@ gulp.task('sass', function () {
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream());
 });
 
 // Configure JS.
@@ -42,13 +33,11 @@ gulp.task('js', function() {
     .pipe(concat('app.js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist/js'))
-    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
   gulp.watch('dist/scss/**/*.scss', ['sass']);
   gulp.watch('dist/js/**/*.js', ['js']);
-  gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['serve', 'sass', 'js']);
+gulp.task('default', ['sass', 'js']);
